af213b4e028c5fc5e73cc583f80dbff4ca28a4cb
kimtuan <kimtuan09112004@gmail.com>
2025-07-08 15:59:12 +0700

    fix: create post without param  userId

--- src/router/posts.ts
+++ src/router/posts.ts
@@ -1,10 +1,24 @@
 import express from "express";
-import { createPost, getHomePosts, getPostById, getPostsByUser } from "../controllers/posts";
+import {
+  createPost,
+  getHomePosts,
+  getPostById,
+  getPostsByUser,
+} from "../controllers/posts";
 import { authenticateToken, uploadImageMiddleware } from "../middlewares";
 
 export const PostRouter = (router: express.Router) => {
-    router.post("/posts/create-post/:userId", authenticateToken, uploadImageMiddleware, createPost);
-    router.get("/posts/get-posts-by-user/:userId", authenticateToken, getPostsByUser);
-    router.get("/posts/get-post-by-id/:postId", authenticateToken, getPostById);
-    router.get("/posts/get-home-posts", authenticateToken, getHomePosts);
-}
+  router.post(
+    "/posts/create-post",
+    authenticateToken,
+    uploadImageMiddleware,
+    createPost
+  );
+  router.get(
+    "/posts/get-posts-by-user/:userId",
+    authenticateToken,
+    getPostsByUser
+  );
+  router.get("/posts/get-post-by-id/:postId", authenticateToken, getPostById);
+  router.get("/posts/get-home-posts", authenticateToken, getHomePosts);
+};
